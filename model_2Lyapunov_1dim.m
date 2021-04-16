50ae79aba108d6e4ee8256e8ded878495f1f62e9
Masahiro Ito <itomasa@lim.sc.e.titech.ac.jp>
2021-04-17 00:08:55 +0900

    適応制御(2018 コロナ社) の第二章の数値例を実装した

---
+++ model_2Lyapunov_1dim.m
@@ -0,0 +1,19 @@
+% 一次系に対するモデル規範型適応制御系（リアプノフ関数方式）
+function dx=model_2Lyapunov_1dim(x,param,r)
+  % x=[y,ym,theta1,theta2]
+  % param=[a;b;am;bm;alpha1;alpha2]
+  % eta1=r/(s+am) eta2=y/(s+am)
+  % r=@(x) sin(x)
+
+  x_cell=num2cell(x);
+  [y,ym,theta1,theta2]=x_cell{:}; 
+  param_cell=num2cell(param);
+  [a,b,am,bm,alpha1,alpha2]=param_cell{:};
+  
+  u=theta1*r+theta2*y;
+  e=ym-y;
+  
+  
+  dx=[-a*y+b*u;-am*ym+bm*r;alpha1*r*e;alpha2*y*e];
+
+end
